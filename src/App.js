b6fdd5f75776039ad62e6e4ec5efee93ed4c3840
Ivan Nikitin <iva3nik@yandex.ru>
2022-05-04 15:36:12 +0300

    feat: add Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import s from "./App.module.scss";
 
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className={s.app}>
       <Header />
+
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import logo_big from "../../assets/svg/logo_big.svg";
+import telegram from "../../assets/svg/icon_telegram.svg";
+import vk from "../../assets/svg/icon_vk.svg";
+import twitter from "../../assets/svg/icon_twitter.svg";
+import h from "../../assets/svg/icon_h.svg";
+
+import s from "./Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <footer className={s.footer}>
+      <div className={s.footer__logoColumn}>
+        <img className={s.footer__logo} src={logo_big} alt="Logo" />
+        <p>© ООО «Лого», 2008—2022</p>
+      </div>
+      <div className={s.footer__column}>
+        <div className={s.footer__links}>
+          <Link className={s.footer__link} to="#">
+            Решения
+          </Link>
+          <Link className={s.footer__link} to="#">
+            Оборудование
+          </Link>
+          <Link className={s.footer__link} to="#">
+            О компании
+          </Link>
+          <Link className={s.footer__link} to="#">
+            Блог
+          </Link>
+        </div>
+        <div className={s.footer__list}>
+          <a
+            href="#"
+            className={s.footer__item}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={telegram} alt="Telegram icon" />
+          </a>
+          <a
+            href="#"
+            className={s.footer__item}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={vk} alt="Vk icon" />
+          </a>
+          <a
+            href="#"
+            className={s.footer__item}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={twitter} alt="Twitter icon" />
+          </a>
+          <a
+            href="#"
+            className={s.footer__item}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={h} alt="H icon" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
