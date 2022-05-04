8e81a82e0ae436de1e3d0d2d4fd79ef8b90532b4
Ivan Nikitin <iva3nik@yandex.ru>
2022-05-04 14:34:38 +0300

    feat: add adaptive for Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 import logo from "../../assets/svg/logo.svg";
+import phone from "../../assets/svg/phone.svg";
+import envelop from "../../assets/svg/envelop.svg";
 
 import s from "./Header.module.scss";
 
@@ -11,6 +13,8 @@ const Header = () => {
       <div className={s.header__info}>
         <p className={s.header__text}>8 800 000 00 00</p>
         <p className={s.header__text}>sales@logo.ru</p>
+        <img className={s.header__icon} src={phone} alt="Phone icon" />
+        <img className={s.header__icon} src={envelop} alt="Envelop icon" />
       </div>
     </header>
   );
